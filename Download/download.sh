5bbf0e3e0f26f586e601a7a0e70f0efcecb72714
PMC-kydangthe <59247379+PMC-kydangthe@users.noreply.github.com>
2022-08-23 11:41:53 +0700

    Update download.sh

--- Download/download.sh
+++ Download/download.sh
@@ -1,8 +1,8 @@
 
 echo ${PWD}
 
-wget --no-verbose https://archive.apache.org/dist/spark/spark-3.3.0/spark-3.3.0-bin-hadoop3-scala2.13.tgz
+wget --no-verbose https://archive.apache.org/dist/spark/spark-3.3.0/spark-3.3.0-bin-hadoop2.tgz
 
-cp spark-3.3.0-bin-hadoop3-scala2.13.tgz ../airflow/spark-3.3.0-bin-hadoop3-scala2.13.tgz
-cp spark-3.3.0-bin-hadoop3-scala2.13.tgz ../spark/spark-3.3.0-bin-hadoop3-scala2.13.tgz
-cp spark-3.3.0-bin-hadoop3-scala2.13.tgz ../jupyter/spark-3.3.0-bin-hadoop3-scala2.13.tgz
+cp spark-3.3.0-bin-hadoop2.tgz ../airflow/spark-3.3.0-bin-hadoop3-scala2.13.tgz
+cp spark-3.3.0-bin-hadoop2.tgz ../spark/spark-3.3.0-bin-hadoop3-scala2.13.tgz
+cp spark-3.3.0-bin-hadoop2.tgz ../jupyter/spark-3.3.0-bin-hadoop3-scala2.13.tgz
